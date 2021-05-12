c2dcd4c8d3a061e23f20a182928fbc26bf891005
Juan Bedoya <juan.bedoya@psl.com.co>
2021-05-12 13:43:11 -0500

    fix: show only contracts belonging to the logged-in profile in contract by id endpoint

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
 const app = express();
+const { Op } = require("sequelize");
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const { id: profileId} = req.profile;
+
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Op.or]: [
+                { ClientId: profileId },
+                { ContractorId: profileId },
+            ]
+        }
+    })
+
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
 module.exports = app;
