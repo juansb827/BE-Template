f177ddce34ba790f0e7170b360421a252f70a543
Juan Bedoya <juan.bedoya@psl.com.co>
2021-05-12 13:41:26 -0500

    fix: correctly handle errors on app startup

--- src/server.js
+++ src/server.js
@@ -2,13 +2,11 @@ const app = require('./app');
 
 init();
 
-async function init() {
-  try {
+function init() {
     app.listen(3001, () => {
       console.log('Express App Listening on Port 3001');
+    }).on('error', (error) => {
+      console.error(`An error occurred: ${JSON.stringify(error)}`);
+      process.exit(1);
     });
-  } catch (error) {
-    console.error(`An error occurred: ${JSON.stringify(error)}`);
-    process.exit(1);
-  }
 }
